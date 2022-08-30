9d3acce3f9123c70a99cf7f2d51e0238bef751c3
SH <https://github.com/miracleSH>
2022-08-30 19:17:30 +0900

    프로젝트 생성

---
+++ src/main/java/com/peacizen/peacizen_api/PeacizenApiApplication.java
@@ -0,0 +1,13 @@
+package com.peacizen.peacizen_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeacizenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeacizenApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peacizen/peacizen_api/PeacizenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peacizen.peacizen_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeacizenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
